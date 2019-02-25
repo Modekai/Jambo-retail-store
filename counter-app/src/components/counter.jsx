552347712a23acaa6f58683b61f1ad43e29b4152
Modekai <muzyar.rad@gmail.com>
2019-02-24 19:20:53 -0800

    working with state and render

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+    address: {
+      street: ""
+    }
+  };
+
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <React.Fragment>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
